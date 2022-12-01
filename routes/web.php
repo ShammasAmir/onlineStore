bde46d3c4918261e83fcc6b679772bcb63367833
ShammasAmir <amirshammas@gmail.com>
2022-12-01 13:41:46 +0330

    Index view and about view created.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
