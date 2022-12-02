71fb58ff056e63e4168046a72ebb0082bfda08ba
ShammasAmir <amirshammas@gmail.com>
2022-12-02 13:20:02 +0330

    Products table created.

---
+++ database/migrations/first_insert.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO products (id, name, description, image, price) VALUES (NULL, 'TV', 'Best TV', 'game.png', '1000');
+
+INSERT INTO products (id, name, description, image, price) VALUES (NULL, 'iPhone', 'Best iPhone', 'safe.png', '999');
+
+INSERT INTO products (id, name, description, image, price) VALUES (NULL, 'Chromecast', 'Best Chromecast', 'submarine.png', '30');
+
+INSERT INTO products (id, name, description, image, price) VALUES (NULL, 'Glasses', 'Best Glasses', 'game.png', '100');
